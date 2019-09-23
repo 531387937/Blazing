b1b69418b1e1711380be0f3c3c4d1e477b6e9d40
xSCx <xx1012542997@outlook.com>
2019-09-24 01:02:30 +0800

    ##Plate脚本修改

---
+++ FFFamily/Assets/Scripts/Plate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    public float MaxAngle;
+    
+
+    void Awake() 
+    {                                        
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ FFFamily/Assets/Scripts/Plate/PlateControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateControl : MonoBehaviour
+{
+    public float MaxAngle;
+
+    void Start()
+    {
+        WeightCore();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void WeightCore()
+    {
+        float angle;
+        Vector3 Wdirection = new Vector3(0, 0, 1);
+        List<Transform> players = new List<Transform>();
+        GameObject p = gameObject.transform.Find("Players").gameObject;
+        foreach(Transform child in p.transform)
+        {
+            players.Add(child);
+            //Debug.Log(child.gameObject.name);
+        }
+
+        for (int i = 0; i < players.Count; i++)
+        {
+            Wdirection += new Vector3(players[i].gameObject.transform.position.x, players[i].transform.position.z, 0);
+            //Debug.Log(players[i].gameObject + "(" + players[i].gameObject.transform.position.x+"," +players[i].gameObject.transform.position.z+ ")");
+        }
+        angle = (float)System.Math.Sqrt(Wdirection.x * Wdirection.x + Wdirection.y * Wdirection.y);
+    }
+}
