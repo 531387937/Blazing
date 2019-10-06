5a904e3af091707c39d8cfd93012c419bb3b62d0
Qizi <531387937@qq.com>
2019-10-06 20:46:34 +0800

    移动修改为纯物理

--- FFFamily/Assets/Scripts/Plate/PlateControl.cs
+++ FFFamily/Assets/Scripts/Plate/PlateControl.cs
@@ -9,6 +9,7 @@ public class PlateControl : MonoBehaviour
 
     public float rotSpeed;
     public float slideSpeed;
+    List<Transform> players = new List<Transform>();
     private Quaternion targetRot;
     GameObject p;
     private Vector3 jumpAffect;
@@ -30,40 +31,30 @@ public class PlateControl : MonoBehaviour
     void Update()
     {
         targetRot = WeightCore();
-        slideSpeed += 0.02f * Time.deltaTime;
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotSpeed * Time.deltaTime);
 
-        p.transform.localPosition += slideSpeed*slideSpeed * new Vector3(-transform.rotation.z, 0, transform.rotation.x)*Time.deltaTime;
+        //p.transform.localPosition += slideSpeed*slideSpeed * new Vector3(-transform.rotation.z, 0, transform.rotation.x)*Time.deltaTime;
 
-        jumpAffect = Vector3.Slerp(jumpAffect, Vector3.zero, 1 * Time.deltaTime);
+        //jumpAffect = Vector3.Slerp(jumpAffect, Vector3.zero, 1 * Time.deltaTime);
 
-        print(jumpAffect);
     }
 
     Quaternion WeightCore()
     {
         float angle;
         Vector3 Wdirection = new Vector3(0, 0, 1);
-        List<Transform> players = new List<Transform>();
-        //GameObject p = gameObject.transform.Find("Players").gameObject;
         foreach(Transform child in p.transform)
         {
             players.Add(child);
-            //Debug.Log(child.gameObject.name);
         }
 
         for (int i = 0; i < players.Count; i++)
         {
-            PlayerBase Pb;
             float mass = 1;
-            if (m_PlayerManager._players.TryGetValue(players[i].gameObject, out Pb))
-            {
-                mass = Pb.weight;
-            }
-            Wdirection += new Vector3(players[i].gameObject.transform.localPosition.x, players[i].transform.localPosition.z, 0) * mass;
+            Wdirection += new Vector3(players[i].gameObject.transform.position.x, players[i].transform.position.z, 0) * mass;
             //Debug.Log(players[i].gameObject + "(" + players[i].gameObject.transform.position.x+"," +players[i].gameObject.transform.position.z+ ")");
         }
-        Wdirection += jumpAffect;
+        //Wdirection += jumpAffect;
         angle = Mathf.Sqrt(Wdirection.x * Wdirection.x + Wdirection.y * Wdirection.y);
         Vector3 from = Vector3.up;
         Vector3 to = new Vector3(-Wdirection.x, test, Wdirection.y);
@@ -76,22 +67,34 @@ public class PlateControl : MonoBehaviour
 
     private void OnCollisionExit(Collision collision)
     {
-        print(collision.gameObject.name);
-        PlayerBase pb;
-        if(m_PlayerManager._players.TryGetValue(collision.gameObject,out pb))
+        if (collision.gameObject.CompareTag("Player") && players.Contains(collision.transform))
         {
-            pb.state = playerState.Jump;
+            players.Remove(collision.transform);
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        print("Enter");
-        PlayerBase pb;
-        if (m_PlayerManager._players.TryGetValue(collision.gameObject, out pb))
+        //print("Enter");
+        //PlayerBase pb;
+        //if (m_PlayerManager._players.TryGetValue(collision.gameObject, out pb))
+        //{
+        //    pb.state = playerState.OnGround;
+        //    jumpAffect += -pb.maxVelocity * 1.3f * new Vector3(collision.gameObject.transform.localPosition.x, collision.gameObject.transform.localPosition.z, 0);
+        //    //print(pb.jumpForce * 3 * new Vector3(collision.gameObject.transform.localPosition.x, collision.gameObject.transform.localPosition.z, 0));
+        //}
+        if (collision.gameObject.CompareTag("Player") && !players.Contains(collision.transform))
         {
-            pb.state = playerState.OnGround;
-            jumpAffect += -pb.maxVelocity * 1.3f * new Vector3(collision.gameObject.transform.localPosition.x, collision.gameObject.transform.localPosition.z, 0);
-            //print(pb.jumpForce * 3 * new Vector3(collision.gameObject.transform.localPosition.x, collision.gameObject.transform.localPosition.z, 0));
+            players.Add(collision.transform);
+            collision.gameObject.GetComponent<Players>().state = playerState.OnGround;
         }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //if(collision.gameObject.CompareTag("Player")&&!players.Contains(collision.transform))
+        //{
+        //    players.Add(collision.transform);
+        //    collision.gameObject.GetComponent<Players>().state = playerState.OnGround;
+        //}
     }
 }
